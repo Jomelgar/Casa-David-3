cf63e1d5d1c8ee10983888d22ab0b5ad148ddd6d
Johnny Melgar <Jomelgar1903@gmail.com>
2025-06-14 09:02:42 -0600

    Validar userlog

--- client/src/utilities/validarUserLog.js
+++ client/src/utilities/validarUserLog.js
@@ -2,7 +2,7 @@ export function validarPrivilegio(user, privilegio) {
   if (!user) return false;
   if(user.role === "master") return true;
   if (privilegio === 11) return false;
-  
+  if(user.role === "admin") return true;
   const valid = user.privilegios.includes(privilegio)
   console.log("User incluye privilegio: ", valid)
   return valid;
